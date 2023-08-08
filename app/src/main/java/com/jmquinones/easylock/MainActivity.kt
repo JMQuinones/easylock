57b34a164153d9244d2672e883fddcc26a7ddb77
JMQuinones <quinones.v.j.maurcio@gmail.com>
2023-08-08 10:20:08 -0400

    changed main activity

--- app/src/main/java/com/jmquinones/easylock/MainActivity.kt
+++ app/src/main/java/com/jmquinones/easylock/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.jmquinones.easylock
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnContinue: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initComponents()
+        initListeners()
+    }
+
+    private fun initComponents() {
+        btnContinue = findViewById(R.id.btnContinue)
+    }
+
+    private fun initListeners() {
+        btnContinue.setOnClickListener{
+            val intent = Intent(this, MainMenuActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
