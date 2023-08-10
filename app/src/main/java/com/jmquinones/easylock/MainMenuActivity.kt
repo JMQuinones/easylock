c782ec7481e5884bc4e4cba4b36690c1461e9376
JMQuinones <quinones.v.j.maurcio@gmail.com>
2023-08-10 14:52:26 -0400

    added enroll alert dialog and api level verification

--- app/src/main/java/com/jmquinones/easylock/MainMenuActivity.kt
+++ app/src/main/java/com/jmquinones/easylock/MainMenuActivity.kt
@@ -1,13 +1,13 @@
 package com.jmquinones.easylock
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.content.Intent
-import java.util.concurrent.Executor
+import android.os.Bundle
 import android.provider.Settings
-
 import android.util.Log
 import android.widget.Toast
-
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
 import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_WEAK
@@ -15,9 +15,9 @@ import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
 import androidx.biometric.BiometricPrompt.PromptInfo
 import androidx.core.content.ContextCompat
-
-import android.os.Bundle
 import com.jmquinones.easylock.databinding.ActivityMainMenuBinding
+import java.util.concurrent.Executor
+
 
 class MainMenuActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainMenuBinding
@@ -44,7 +44,11 @@ class MainMenuActivity : AppCompatActivity() {
             biometricPrompt.authenticate(promptInfo)
 
         }
+        binding.cvFace.setOnClickListener {
+            showDialog()
+        }
     }
+
 
     private fun checkDeviceHasBiometric() {
         val biometricManager = BiometricManager.from(this)
@@ -68,12 +72,20 @@ class MainMenuActivity : AppCompatActivity() {
 
             }
             BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+
+                showDialog()
+//                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q){
+//                    val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+//                        putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+//                            BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+//                    }
+//                    startActivityForResult(enrollIntent, 100)
+//                } else{
+//                    val enrollIntent = Intent(Settings.ACTION_SECURITY_SETTINGS)
+//                    startActivityForResult(enrollIntent, 100)
+//                }
                 // Prompts the user to create credentials that your app accepts.
-                val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
-                    putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
-                        BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
-                }
-                startActivityForResult(enrollIntent, 100)
+
             }
             else ->{
                 Log.e("MY_APP_TAG", "Biometric features are currently unavailable.")
@@ -83,6 +95,34 @@ class MainMenuActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun showDialog() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setCancelable(true)
+        builder.setTitle("No se encontraron registros biometricos")
+        builder.setMessage("Abrir seguridad para configurar?")
+        builder.setPositiveButton("Abrir",
+            DialogInterface.OnClickListener { dialog, which -> openSettings()})
+        builder.setNegativeButton("Cancelar",
+            DialogInterface.OnClickListener { dialog, which -> })
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun openSettings() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q){
+            val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                    BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+            }
+            startActivityForResult(enrollIntent, 100)
+        } else{
+            val enrollIntent = Intent(Settings.ACTION_SECURITY_SETTINGS)
+            startActivityForResult(enrollIntent, 100)
+        }
+    }
+
     private fun buildPromtInfo() = PromptInfo.Builder()
         .setTitle("Autenticación biómetrica")
         .setSubtitle("Coloque su dedo en el sensor")
