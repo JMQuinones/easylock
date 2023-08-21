b5e02269e8cf3b203dcd683619eaa34d5dd697bd
JMQuinones <quinones.v.j.maurcio@gmail.com>
2023-08-21 11:01:56 -0400

    added placeholder tflite model functionality

--- app/src/main/java/com/jmquinones/easylock/CameraActivity.kt
+++ app/src/main/java/com/jmquinones/easylock/CameraActivity.kt
@@ -5,15 +5,22 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.media.ThumbnailUtils
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.jmquinones.easylock.databinding.ActivityCameraBinding
+import com.jmquinones.easylock.ml.Model1
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class CameraActivity : AppCompatActivity() {
-    lateinit var binding: ActivityCameraBinding
+    private lateinit var binding: ActivityCameraBinding
+    private var imageSize:Int =224
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityCameraBinding.inflate(layoutInflater)
         val view = binding.root
@@ -45,28 +52,74 @@ class CameraActivity : AppCompatActivity() {
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
                 val imageBitmap = data?.extras?.getParcelable("data", Bitmap::class.java) as Bitmap
-                binding.ivPicture.setImageBitmap(imageBitmap)
+//                binding.ivPicture.setImageBitmap(imageBitmap)
+                processImage(imageBitmap)
             } else {
                 val imageBitmap = data?.extras?.get("data") as Bitmap
-                binding.ivPicture.setImageBitmap(imageBitmap)
+//                binding.ivPicture.setImageBitmap(imageBitmap)
+                processImage(imageBitmap)
+
             }
 //            binding.ivPicture.setImageBitmap(imageBitmap)
         }
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            binding.ivPicture.setImageBitmap(imageBitmap)
-
-//            val dimension = image!!.width.coerceAtMost(image.height)
-//            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension)
-//            binding.ivPicture.setImageBitmap(image)
-//            Log.d("BITMAP", image.toString())
-//            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false)
-//            classifyImage(image)
-        }
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun processImage(imageBitmap: Bitmap) {
+//        binding.ivPicture.setImageBitmap(imageBitmap)
+        var image: Bitmap = imageBitmap
+        val dimension = image.width.coerceAtMost(image.height)
+        image = ThumbnailUtils.extractThumbnail(image, dimension, dimension)
+        binding.ivPicture.setImageBitmap(image)
+        image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false)
+            classifyImage(image)
     }
+
+    private fun classifyImage(image: Bitmap?) {
+        val model = Model1.newInstance(applicationContext);
+
+        // Creates inputs for reference.
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+
+        val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+        byteBuffer.order(ByteOrder.nativeOrder())
+
+        val intValues = IntArray(imageSize * imageSize)
+        image!!.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
+        var pixel = 0
+//        Get RGB values from the bitmap
+        for (i in 0 until imageSize) {
+            for (j in 0 until imageSize) {
+                val `val` = intValues[pixel++] // RGB
+                byteBuffer.putFloat((`val` shr 16 and 0xFF) * (1f / 255f))
+                byteBuffer.putFloat((`val` shr 8 and 0xFF) * (1f / 255f))
+                byteBuffer.putFloat((`val` and 0xFF) * (1f / 255f))
+            }
+        }
+        inputFeature0.loadBuffer(byteBuffer)
+
+        // Runs model inference and gets result.
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        val confidences = outputFeature0.floatArray
+        var maxConfidence = 0f
+        var maxPos = 0
+        for (i in confidences.indices) {
+            if (confidences[i] > maxConfidence) {
+                maxConfidence = confidences[i]
+                maxPos = i
+            }
+        }
+        val classes = arrayOf("Banana", "orange", "Pen", "Stikcy Note")
+        binding.tvPrediction.text = classes[maxPos]
+        var s = ""
+        for (i in classes.indices) {
+            s += String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100)
+        }
+        binding.tvConfidence.text = s
+
+        // Releases model resources if no longer used.
+        model.close()
+    }
+
 
 }
